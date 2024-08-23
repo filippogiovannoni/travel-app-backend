ff6fbc5d8ae561de0f48265a5782f8a633144439
filippogiovannoni <filippogiovannonii@gmail.com>
2024-08-23 16:02:42 +0200

    Created Days model with controller, migration and CRUD

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\DayController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,5 +33,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// CRUD
+Route::resource('days', DayController::class);
 
 require __DIR__ . '/auth.php';
